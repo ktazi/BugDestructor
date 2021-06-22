691e2f1d445c774488a21a52dc4ec260a5feb795
Charlotte <charlotte.mauvezin@gmail.com>
2021-06-22 11:46:36 +0200

    Let's rock

--- Dataset creation/shuffler.py
+++ Dataset creation/shuffler.py
@@ -8,14 +8,17 @@ def shake_my_sent(t):
     t.pop(-1)
     sem = 1
 
-  random.shuffle(tab)
-
+  random.shuffle(t)
+  print(t)
   for i in range(len(t)):
     t[i] = t[i] + " "
 
   if sem == 1:
     t.append(";")
 
+def suprem_check(tt):
+  for i in range(len(tt)):
+    shake_my_sent(tt[i])
 
 def print_tab_lines(t):
   for mot in t:
@@ -26,13 +29,23 @@ def make_sent(t):
   return ("".join(t))
 
 
-tab = ["a", "=", "c", "+", "d", ";"]
-tap = ["a", "=", "c", "+", "d"]
+def write_sents(t):
+  pouet = ''
+  nameSave = "clearLines.txt"
+  for i in range(len(t)):
+    pouet = make_sent(t[i])
+    file = open(nameSave, "a")
+    file.write(pouet)
+    file.write("\n")
 
-shake_my_sent(tab)
-pouet = make_sent(tab)
 
-nameSave = "clearLines.txt"
-file = open(nameSave, "a")
-file.write(pouet)
+tab = [["a", "=", "c", "+", "d", ";"], ["a", "=", "c", "+", "d"], ["if", "a", "<", "b", "{"]]
+#tap = ["a", "=", "c", "+", "d"]
+#tac = ["if", "a", "<", "b", "{"]
+
+suprem_check(tab)
+print(tab)
+
+
+
 
